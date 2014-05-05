78d67dd8ee32cff08bb845962c32321f90fbe81d
piotr <d3v@o2.pl>
2014-05-06 01:34:20 +0200

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Jumanji Cinema!"
       redirect_to @user
     else
