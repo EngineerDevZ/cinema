f7b0d5f986ef2a582da57fd56cb64238687b4955
piotr <d3v@o2.pl>
2014-05-17 14:10:08 +0200

    Add genre

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140505230813) do
+ActiveRecord::Schema.define(version: 20140517113200) do
+
+  create_table "genres", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
