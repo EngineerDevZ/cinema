efe295a5143bce28f9499fdb22759cb978690f41
piotr <d3v@o2.pl>
2014-06-11 02:11:44 +0200

    Update 1

--- app/models/showing.rb
+++ app/models/showing.rb
@@ -6,10 +6,18 @@ class Showing < ActiveRecord::Base
   has_many :ticket_seat_showings
   validates :price, presence: true, numericality: { greater_than: 0}
   validates :date, presence: true
-  validates :date, presence: true
-  validates :is3d, presence: true
+  validates_inclusion_of :is3d, :in => [true, false]
   validates :vat, presence: true, numericality: { greater_than: 0}
   validates :movie_id, presence: true, numericality: { greater_than: 0}
   validates :room_id, presence: true, numericality: { greater_than: 0}
   validates :showing_status_id, presence: true, numericality: { greater_than: 0}
+  validate :validate_date
+
+  def validate_date
+    if date
+      if date.year > 2100
+        errors.add(:date, "has wrong year")
+      end
+    end
+  end
 end
