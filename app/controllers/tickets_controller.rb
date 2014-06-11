89e10dd764bb3717f56161734c147aecde01f159
piotr <d3v@o2.pl>
2014-06-11 03:07:22 +0200

    Update 2

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -18,11 +18,13 @@ class TicketsController < ApplicationController
     session[:psession_id] = @session_id
     @ticket = Ticket.new(showing_id: params[:id1], user_id: @current_user, discount_id: params[:id3], ticket_status_id: 1)
     @ticket_seat_showing = TicketSeatShowing.new(seat_id: params[:id2], showing_id: @showing)
+    @ticket_crc = Digest::SHA1.hexdigest(@session_id.to_s + ((@ticket.showing.price - (@ticket.showing.price * (@ticket.discount.value/100.0))) * 100).to_int.to_s)
+    session[:ticket_crc] = @ticket_crc
   end
   
   def confirm_ok
-	#p24_crc = Digest::MD5::hexdigest(params[:p24_session_id].to_s + "|27463|" + params[:p24_kwota].to_s + "|" + params[:p24_crc].to_s)
-    if params[:p24_session_id] == session[:psession_id]
+    @crc_ticket = Digest::SHA1.hexdigest(params[:p24_session_id].to_s + params[:p24_kwota].to_s)
+    if params[:p24_session_id] == session[:psession_id] && @crc_ticket == session[:ticket_crc]
       url = URI.parse('http://sandbox.przelewy24.pl/transakcja.php')
       http = Net::HTTP.new(url.host, url.port)
       http.request_post url.path, "p24_kwota=" + params[:p24_kwota] + "&p24_order_id=" + params[:p24_order_id] + "&p24_id_sprzedawcy=27463&p24_session_id=" + params[:p24_session_id] + "&p24_crc=" + params[:p24_crc]
